e2fff5c4e7572b16ee6e92109a341df20b237c74
kiwoonjung <kiwoon0627@gmail.com>
2022-12-14 19:59:45 -0800

    add default avatar image and username on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,47 @@
 import "../Header/Header.scss";
-// import menu from "../../assets/images/icons/menu.svg";
-// import user from "../../assets/images/icons/user.svg";
 import { Link } from "react-router-dom";
+import { useState, useEffect, useReducer } from "react";
+import axios from "axios";
 
 export default function Header() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [userAvatar, setUserAvatar] = useState("");
+
+  /*
+   * Component Mount, check if localStorage has JWT token
+   * if token exists verify JWT and login user
+   */
+  useEffect(() => {
+    const jwtToken = localStorage.getItem("jwt_token");
+    // if JWT token exists try to load the user profile, user object
+    if (jwtToken) {
+      loadProfile(jwtToken);
+    }
+  }, []);
+
+  /*
+   * Get user data
+   * send JWT token as part of request headers
+   * token is decoded on the server and if valid sends back a user object
+   */
+  const loadProfile = (jwtToken) => {
+    axios
+      .get("http://localhost:8080/api/auth/users", {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      })
+      .then((response) => {
+        setLoggedIn(true);
+        setUser(response.data[0].name);
+        setUserAvatar("http://localhost:8080" + response.data[0].imageUrl);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="header-wrapper">
       <div className="header-container">
@@ -14,6 +52,16 @@ export default function Header() {
                 <div>Logo</div>
               </div>
             </Link>
+            <div className="header-user-container">
+              <div className="header-avatar-container">
+                <img
+                  className="user-avatar"
+                  src={userAvatar}
+                  alt="user-avatar"
+                />
+              </div>
+              <div className="header-user">{user}</div>
+            </div>
             <input className="header-input" type="checkbox" id="hamburger1" />
             <label className="header-label" htmlFor="hamburger1"></label>
 
